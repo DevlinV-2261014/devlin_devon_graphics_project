b087fba9afb8f8f1427b15ea10da81594e4741e7
DevlinV-2261014 <devlin.vranken@student.uhasselt.be>
2023-04-20 14:24:25 +0200

    Somehow had some stuff that needed to be pushed
    After I pushed, some stuff that seems important showed itself to push. Therefor, here it is.

--- Devlin_Devon_Computer_Graphics_Project/Main.cpp
+++ Devlin_Devon_Computer_Graphics_Project/Main.cpp
@@ -11,15 +11,18 @@ void processInput(GLFWwindow* window);
 
 int main() {
 
+	// Read the maze layout into a string (Not needed anymore
 	/*string layout{ readMazeLayout("maze.txt") };
 	cout << layout << endl;*/
 
+	// Read the maze file and create positions for each #
 	vector<glm::vec3> cubeLocations = getMazeLayout("maze.txt");
 	/*for (glm::vec3 vec : cubeLocations) {
 		cout << vec.x << " " << vec.y << " " << vec.z << endl;
 		;
 	}*/
 
+	// Initialize the window
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
@@ -44,6 +47,7 @@ int main() {
 
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
+	// Rendering in loop
 	while (!glfwWindowShouldClose(window))
 	{
 		// input
@@ -67,6 +71,7 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 	glViewport(0, 0, width, height);
 }
 
+// Process keybinds
 void processInput(GLFWwindow* window)
 {
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)

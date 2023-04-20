8d0c116606c2bf8bcd6734a5a0bb6992ddd8497a
DevlinV-2261014 <devlin.vranken@student.uhasselt.be>
2023-04-20 12:52:56 +0200

    Create solution for the project
    The project is now a solution instead of just a folder.

---
+++ Devlin_Devon_Computer_Graphics_Project/FileReader.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+string readMazeLayout(string fileName) {
+	string layout;
+	fstream mazeFile;
+
+	mazeFile.open(fileName, ios::in);
+	if (mazeFile.is_open()) {
+		cout << "reading file: " << fileName << endl;
+		while (getline(mazeFile, layout)) {
+			cout << layout << endl;
+		}
+		mazeFile.close();
+	}
+	else {
+		cout << "Unable to read file: " << fileName << endl;
+	}
+
+	return layout;
+}
---
+++ Devlin_Devon_Computer_Graphics_Project/Main.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <string>
+#include "FileReader.h"
+using namespace std;
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
+
+int main() {
+
+	string layout{ readMazeLayout("maze.txt") };
+	cout << layout << endl;
+
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
+	glViewport(0, 0, 800, 600);
+
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		// input
+		processInput(window);
+
+		// rendering commands here
+		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		// check and call events and swap the buffers
+		glfwPollEvents();
+		glfwSwapBuffers(window);
+	}
+
+	glfwTerminate();
+	return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+}

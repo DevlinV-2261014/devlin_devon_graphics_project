5ca965a9502c5c6e925375fdd66ec675a2b7f589
DevonVanaenrodePXL <devon.vanaenrode@student.uhasselt.be>
2023-04-24 14:37:32 +0200

    Push this ugly ass solution to all our problems

--- Devlin_Devon_Computer_Graphics_Project/Main.cpp
+++ Devlin_Devon_Computer_Graphics_Project/Main.cpp
@@ -100,6 +100,23 @@ int main() {
 		cout << vec.x << " " << vec.y << " " << vec.z << endl;
 	}*/
 
+	// this needs to be removed later PLAGIAAT
+	// world space positions of our cubes
+	cubeLocations = {
+		glm::vec3(0.0f,  0.0f,  0.0f),
+		glm::vec3(2.0f,  5.0f, -15.0f),
+		glm::vec3(-1.5f, -2.2f, -2.5f),
+		glm::vec3(-3.8f, -2.0f, -12.3f),
+		glm::vec3(2.4f, -0.4f, -3.5f),
+		glm::vec3(-1.7f,  3.0f, -7.5f),
+		glm::vec3(1.3f, -2.0f, -2.5f),
+		glm::vec3(1.5f,  2.0f, -2.5f),
+		glm::vec3(1.5f,  0.2f, -1.5f),
+		glm::vec3(-1.3f,  1.0f, -1.5f)
+	};
+
+
+
 	// Initialize the window
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -178,7 +195,7 @@ int main() {
 	}
 
 	stbi_image_free(data);
-	
+
 	// Use the shaders
 	mazeShader.use();
 	mazeShader.setInt("wallTexture", 0);
@@ -187,12 +204,18 @@ int main() {
 	// Rendering in loop
 	while (!glfwWindowShouldClose(window))
 	{
+		// per-frame time logic PLAGIAAT
+		// --------------------
+		float currentFrame = static_cast<float>(glfwGetTime());
+		deltaTime = currentFrame - lastFrame;
+		lastFrame = currentFrame;
+		
 		// input
 		processInput(window);
 
 		// rendering commands here
-		glClearColor(0.3f, 0.5f, 0.4f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		// bind textures
 		glActiveTexture(GL_TEXTURE0);
@@ -200,24 +223,23 @@ int main() {
 
 		mazeShader.use();
 		glm::mat4 projection = glm::perspective(glm::radians(cameraFov), (float)SCREEN_WIDTH / (float)SCREEN_HEIGHT, 0.1f, 100.0f);
-		glm::mat4 view = glm::lookAt(cameraPosition, cameraPosition + cameraFront, cameraUp);
-
 		mazeShader.setMat4("projection", projection);
+		glm::mat4 view = glm::lookAt(cameraPosition, cameraPosition + cameraFront, cameraUp);
 		mazeShader.setMat4("view", view);
+		
 
+		// render boxes PLAGIAAT
 		glBindVertexArray(VAO);
-		// For every cube we have
-		for (glm::vec3 location : cubeLocations) {
-			glm::mat4 model = glm::mat4(1.0f);
-			model = glm::translate(model, location);
-			model = glm::rotate(model, glm::radians(0.0f), glm::vec3(1.0f, 0.3f, 0.5f));
+		for (unsigned int i = 0; i < 10; i++)
+		{
+			// calculate the model matrix for each object and pass it to shader before drawing
+			glm::mat4 model = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
+			model = glm::translate(model, cubeLocations[i]);
+			float angle = 20.0f * i;
+			model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
 			mazeShader.setMat4("model", model);
-
-			// draw the cube
 			glDrawArrays(GL_TRIANGLES, 0, 36);
 		}
-
-
 		// check and call events and swap the buffers
 		glfwSwapBuffers(window);
 		glfwPollEvents();
--- Devlin_Devon_Computer_Graphics_Project/walls.fs
+++ Devlin_Devon_Computer_Graphics_Project/walls.fs
@@ -4,9 +4,8 @@ out vec4 FragColor;
 in vec2 TexCoord;
 
 uniform sampler2D texture1;
-uniform sampler2D texture2;
 
 void main()
 {
-    FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), 0.2);
+    FragColor = texture(texture1, TexCoord);
 }

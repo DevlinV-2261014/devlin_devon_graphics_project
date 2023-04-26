bf98f75ff5dd105940be7715d0fbef633f2583f5
DevlinV-2261014 <devlin.vranken@student.uhasselt.be>
2023-04-26 13:48:32 +0200

    Moved texture stuff to their own file
    Everything that has to do with textures is now moved to it's own file. This makes the main a little smaller again.

---
+++ Devlin_Devon_Computer_Graphics_Project/TextureHandler.h
@@ -0,0 +1,5 @@
+#pragma once
+#include <string>
+#include <vector>
+unsigned int createTexture(std::string image);
+unsigned int createCubemap(std::vector<std::string> skyBoxFaces);
---
+++ Devlin_Devon_Computer_Graphics_Project/textureHandler.cpp
@@ -0,0 +1,68 @@
+#include "TextureHandler.h"
+#include <glad/glad.h>
+#include <iostream>
+#ifndef STB_IMAGE_IMPLEMENTATION
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+#endif // !STB_IMAGE_IMPLEMENTATION
+using namespace std;
+
+unsigned int createTexture(string image)
+{
+	unsigned int texture;
+	glGenTextures(1, &texture);
+	glBindTexture(GL_TEXTURE_2D, texture);
+
+	// Wrap
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	// Filter
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	int width;
+	int height;
+	int channels;
+	stbi_set_flip_vertically_on_load(true);
+	unsigned char* data = stbi_load("wall.png", &width, &height, &channels, 0);
+
+	if (data) {
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else {
+		cout << "Cannot load the texture for the walls!" << endl;
+	}
+
+	stbi_image_free(data);
+	return texture;
+}
+
+unsigned int createCubemap(vector<string> skyBoxFaces) {
+	unsigned int skyBoxId;
+	glGenTextures(1, &skyBoxId);
+	glBindTexture(GL_TEXTURE_CUBE_MAP, skyBoxId);
+
+	int width, height, nrChannels;
+	for (unsigned int i = 0; i < skyBoxFaces.size(); i++)
+	{
+		unsigned char* data = stbi_load(skyBoxFaces[i].c_str(), &width, &height, &nrChannels, 0);
+		if (data)
+		{
+			glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+			stbi_image_free(data);
+		}
+		else
+		{
+			cout << "Cannot load cubemap texture: " << skyBoxFaces[i] << endl;
+			stbi_image_free(data);
+		}
+	}
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+
+	return skyBoxId;
+}
